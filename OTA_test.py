fe1f990834cd46d0bbceb5f11022c87305db3ff5
Palmen98 <simon.palm98@hotmail.se>
2022-03-29 14:57:51 +0200

    Changing led.

--- OTA_test.py
+++ OTA_test.py
@@ -6,9 +6,5 @@ import time
 def deployTest():
     pycom.heartbeat(False)
     #colors in hexadecimal (0xRRGGBB)
-    pycom.rgbled(0xFF0000)  # Red
-    time.sleep(3)
     pycom.rgbled(0x00FF00)  # Green
-    time.sleep(3)
-    pycom.heartbeat(False)
 
